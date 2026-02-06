06f4a6703330c55728ec40ddf0ac622cc4e5281c
Akhand Pratap Singh <109938960+sakhand97@users.noreply.github.com>
2026-02-06 17:50:36 +0530

    Update schema.sql

--- Sales-KPI-Dashboard/sql/schema.sql
+++ Sales-KPI-Dashboard/sql/schema.sql
@@ -1,1 +1,26 @@
+CREATE TABLE orders (
+    order_id VARCHAR(50),
+    order_date DATE,
+    ship_date DATE,
+    ship_mode VARCHAR(30),
+
+    customer_id VARCHAR(50),
+    customer_name VARCHAR(100),
+    segment VARCHAR(50),
+    region VARCHAR(50),
+    state VARCHAR(50),
+    city VARCHAR(50),
+
+    product_id VARCHAR(50),
+    category VARCHAR(50),
+    sub_category VARCHAR(50),
+    product_name VARCHAR(150),
+
+    sales DECIMAL(10,2),
+    quantity INT,
+    discount DECIMAL(5,2),
+    profit DECIMAL(10,2),
+
+    PRIMARY KEY (order_id, product_id)
+);
 
